caed89fc7d71ec0982bc3d34749f0dc18c68f29d
Geoffroy Couprie <geo.couprie@gmail.com>
2016-03-07 18:20:02 +0100

    Loop until blocking

--- src/network/socket.rs
+++ src/network/socket.rs
@@ -18,37 +18,49 @@ pub trait SocketHandler {
 
 impl SocketHandler for TcpStream {
   fn socket_read(&mut self,  buf: &mut[u8]) -> (usize, SocketResult) {
-    match self.read(buf) {
-      Ok(0)  => (0, SocketResult::Continue),
-      Ok(sz) => (sz, SocketResult::Continue),
-      Err(e) => match e.kind() {
-        ErrorKind::WouldBlock => (0, SocketResult::WouldBlock),
-        ErrorKind::BrokenPipe => {
-          debug!("broken pipe reading from the socket");
-          (0, SocketResult::Error)
-        },
-        _ => {
-          debug!("not implemented; client err={:?}", e);
-          (0, SocketResult::Error)
-        },
+    let mut size = 0usize;
+    loop {
+      if size == buf.len() {
+        return (size, SocketResult::Continue);
+      }
+      match self.read(&mut buf[size..]) {
+        Ok(0)  => return (size, SocketResult::Continue),
+        Ok(sz) => size +=sz,
+        Err(e) => match e.kind() {
+          ErrorKind::WouldBlock => return (size, SocketResult::WouldBlock),
+          ErrorKind::BrokenPipe => {
+            debug!("broken pipe reading from the socket");
+            return (size, SocketResult::Error)
+          },
+          _ => {
+            debug!("socket_write not implemented; client err={:?}", e);
+            return (size, SocketResult::Error)
+          },
+        }
       }
     }
   }
 
   fn socket_write(&mut self,  buf: &[u8]) -> (usize, SocketResult) {
-    match self.write(buf) {
-      Ok(0)  => (0, SocketResult::Continue),
-      Ok(sz) => (sz, SocketResult::Continue),
-      Err(e) => match e.kind() {
-        ErrorKind::WouldBlock => (0, SocketResult::WouldBlock),
-        ErrorKind::BrokenPipe => {
-          debug!("broken pipe writing to the socket");
-          (0, SocketResult::Error)
-        },
-        _ => {
-          debug!("not implemented; client err={:?}", e);
-          (0, SocketResult::Error)
-        },
+    let mut size = 0usize;
+    loop {
+      if size == buf.len() {
+        return (size, SocketResult::Continue);
+      }
+      match self.write(&buf[size..]) {
+        Ok(0)  => return (size, SocketResult::Continue),
+        Ok(sz) => size += sz,
+        Err(e) => match e.kind() {
+          ErrorKind::WouldBlock => return (size, SocketResult::WouldBlock),
+          ErrorKind::BrokenPipe => {
+            debug!("broken pipe writing to the socket");
+            return (size, SocketResult::Error)
+          },
+          _ => {
+            debug!("socket_write not implemented; client err={:?}", e);
+            return (size, SocketResult::Error)
+          },
+        }
       }
     }
   }
@@ -58,27 +70,39 @@ impl SocketHandler for TcpStream {
 
 impl SocketHandler for NonblockingSslStream<TcpStream> {
   fn socket_read(&mut self,  buf: &mut[u8]) -> (usize, SocketResult) {
-    match self.read(buf) {
-      Ok(0)  => (0, SocketResult::Continue),
-      Ok(sz) => (sz, SocketResult::Continue),
-      Err(NonblockingSslError::WantRead)    => (0, SocketResult::WouldBlock),
-      Err(NonblockingSslError::WantWrite)   => (0, SocketResult::WouldBlock),
-      Err(NonblockingSslError::SslError(e)) => {
-         error!("readable TLS client err={:?}", e);
-         (0, SocketResult::Error)
+    let mut size = 0usize;
+    loop {
+      if size == buf.len() {
+        return (size, SocketResult::Continue);
+      }
+      match self.read(&mut buf[size..]) {
+        Ok(0)  => return (size, SocketResult::Continue),
+        Ok(sz) => size += sz,
+        Err(NonblockingSslError::WantRead)    => return (size, SocketResult::WouldBlock),
+        Err(NonblockingSslError::WantWrite)   => return (size, SocketResult::WouldBlock),
+        Err(NonblockingSslError::SslError(e)) => {
+          error!("readable TLS client err={:?}", e);
+          return (size, SocketResult::Error)
+        }
       }
     }
   }
 
   fn socket_write(&mut self,  buf: &[u8]) -> (usize, SocketResult) {
-    match self.write(buf) {
-      Ok(0)  => (0, SocketResult::Continue),
-      Ok(sz) => (sz, SocketResult::Continue),
-      Err(NonblockingSslError::WantRead)    => (0, SocketResult::WouldBlock),
-      Err(NonblockingSslError::WantWrite)   => (0, SocketResult::WouldBlock),
-      Err(NonblockingSslError::SslError(e)) => {
-         error!("readable TLS client err={:?}", e);
-         (0, SocketResult::Error)
+    let mut size = 0usize;
+    loop {
+      if size == buf.len() {
+        return (size, SocketResult::Continue);
+      }
+      match self.write(&buf[size..]) {
+        Ok(0)  => return (size, SocketResult::Continue),
+        Ok(sz) => size +=sz,
+        Err(NonblockingSslError::WantRead)    => return (size, SocketResult::WouldBlock),
+        Err(NonblockingSslError::WantWrite)   => return (size, SocketResult::WouldBlock),
+        Err(NonblockingSslError::SslError(e)) => {
+          error!("readable TLS client err={:?}", e);
+          return (size, SocketResult::Error)
+        }
       }
     }
   }
