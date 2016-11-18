d04be007cb08f0d1d0ce159fc1aeb01524402c38
Geoffroy Couprie <geo.couprie@gmail.com>
2016-11-18 17:56:32 +0100

    Add minimal example

---
+++ examples/minimal.rs
@@ -0,0 +1,57 @@
+#![allow(unused_variables,unused_must_use)]
+#[macro_use] extern crate log;
+extern crate env_logger;
+extern crate yxorp;
+extern crate openssl;
+extern crate mio;
+
+use std::thread;
+use std::sync::mpsc;
+use yxorp::messages;
+use yxorp::network::{self,ProxyOrder};
+
+fn main() {
+  env_logger::init().unwrap();
+  info!("starting up");
+
+  let config = messages::HttpProxyConfiguration {
+    front: "198.51.100.0:80".parse().unwrap(),
+    ..Default::default()
+  };
+
+  let poll          = mio::Poll::new().unwrap();
+  let (tx, rx)      = mio::channel::channel::<ProxyOrder>();
+  let (sender, rec) = mpsc::channel::<network::ServerMessage>();
+
+  let jg            = thread::spawn(move || {
+    network::http::start_listener(config, sender, poll, rx);
+  });
+
+  let http_front = messages::HttpFront {
+    app_id:     String::from("test"),
+    hostname:   String::from("example.com"),
+    path_begin: String::from("/")
+  };
+  let http_instance = messages::Instance {
+    app_id:     String::from("test"),
+    ip_address: String::from("192.0.2.1"),
+    port:       8080
+  };
+
+  tx.send(network::ProxyOrder::Command(
+    String::from("ID_ABCD"),
+    messages::Command::AddHttpFront(http_front)
+  ));
+
+  tx.send(network::ProxyOrder::Command(
+    String::from("ID_EFGH"),
+    messages::Command::AddInstance(http_instance)
+  ));
+
+  println!("HTTP -> {:?}", rec.recv().unwrap());
+  println!("HTTP -> {:?}", rec.recv().unwrap());
+
+  let _ = jg.join();
+  info!("good bye");
+}
+
