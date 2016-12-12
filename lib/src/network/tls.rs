ca570f9c8aa12b9be1d4fe79c21aaafa908f1f60
Geoffroy Couprie <geo.couprie@gmail.com>
2016-12-12 14:59:42 +0100

    set safer default for the cipher list
    but make it overridable

--- lib/src/network/tls.rs
+++ lib/src/network/tls.rs
@@ -336,19 +336,6 @@ impl<Tx: messages::Sender<ServerMessage>> ServerConfiguration<Tx> {
     let mut context = ctx.unwrap();
 
     context.set_cipher_list(&config.cipher_list);
-    context.set_cipher_list("ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:\
-                               ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:\
-                               ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:\
-                               DHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES256-GCM-SHA384:\
-                               ECDHE-ECDSA-AES128-SHA256:ECDHE-RSA-AES128-SHA256:\
-                               ECDHE-ECDSA-AES128-SHA:ECDHE-RSA-AES256-SHA384:\
-                               ECDHE-RSA-AES128-SHA:ECDHE-ECDSA-AES256-SHA384:\
-                               ECDHE-ECDSA-AES256-SHA:ECDHE-RSA-AES256-SHA:DHE-RSA-AES128-SHA256:\
-                               DHE-RSA-AES128-SHA:DHE-RSA-AES256-SHA256:DHE-RSA-AES256-SHA:\
-                               ECDHE-ECDSA-DES-CBC3-SHA:ECDHE-RSA-DES-CBC3-SHA:\
-                               EDH-RSA-DES-CBC3-SHA:AES128-GCM-SHA256:AES256-GCM-SHA384:\
-                               AES128-SHA256:AES256-SHA256:AES128-SHA:AES256-SHA:DES-CBC3-SHA:\
-                               !DSS");
 
     match DH::get_2048_256() {
       Ok(dh) => context.set_tmp_dh(&dh),
@@ -457,19 +444,6 @@ impl<Tx: messages::Sender<ServerMessage>> ServerConfiguration<Tx> {
     let opt = context.set_options(options);
 
     context.set_cipher_list(cipher_list);
-    context.set_cipher_list("ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:\
-                               ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:\
-                               ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:\
-                               DHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES256-GCM-SHA384:\
-                               ECDHE-ECDSA-AES128-SHA256:ECDHE-RSA-AES128-SHA256:\
-                               ECDHE-ECDSA-AES128-SHA:ECDHE-RSA-AES256-SHA384:\
-                               ECDHE-RSA-AES128-SHA:ECDHE-ECDSA-AES256-SHA384:\
-                               ECDHE-ECDSA-AES256-SHA:ECDHE-RSA-AES256-SHA:DHE-RSA-AES128-SHA256:\
-                               DHE-RSA-AES128-SHA:DHE-RSA-AES256-SHA256:DHE-RSA-AES256-SHA:\
-                               ECDHE-ECDSA-DES-CBC3-SHA:ECDHE-RSA-DES-CBC3-SHA:\
-                               EDH-RSA-DES-CBC3-SHA:AES128-GCM-SHA256:AES256-GCM-SHA384:\
-                               AES128-SHA256:AES256-SHA256:AES128-SHA:AES256-SHA:DES-CBC3-SHA:\
-                               !DSS");
 
     match DH::get_2048_256() {
       Ok(dh) => context.set_tmp_dh(&dh),
