383b6a16b21c24c67b46189419c398066365f81a
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2017-08-22 09:36:17 +0200

    exherbo: generate systemd and configuration files
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

---
+++ os-build/exherbo/generate.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+substitute() {
+    sed -e "s:__BINDIR__:${BINDIR}:"         \
+        -e "s:__SYSCONFDIR__:${SYSCONFDIR}:" \
+        -e "s:__DATADIR__:${DATADIR}:"       \
+        -e "s:__RUNDIR__:${RUNDIR}:"         \
+        -e "s:__SOZU_USER__:root:"           \
+        -e "s:__SOZU_GROUP__:root:"          \
+        "${1}" > "${2}"
+}
+
+mkdir -p generated
+scriptdir="$(dirname "${0}")"
+
+substitute "${scriptdir}"/../systemd/sozu.service.in generated/sozu.service
+substitute "${scriptdir}"/../systemd/sozu.conf.in generated/sozu.conf
+substitute "${scriptdir}"/../config.toml.in generated/config.toml
