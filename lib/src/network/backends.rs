8b5757a6f7d01b0fd5de94b44e02152e7635c94e
Geoffroy Couprie <geo.couprie@gmail.com>
2017-08-29 15:10:32 +0200

    start moving an app's instances list to its own struct

--- lib/src/network/backends.rs
+++ lib/src/network/backends.rs
@@ -8,7 +8,7 @@ use mio::net::TcpStream;
 use network::{AppId,Backend,ConnectionError};
 
 pub struct BackendMap {
-  pub instances: HashMap<AppId, Vec<Rc<RefCell<Backend>>>>,
+  pub instances: HashMap<AppId, BackendList>,
 }
 
 impl BackendMap {
@@ -20,22 +20,22 @@ impl BackendMap {
 
   pub fn add_instance(&mut self, app_id: &str, instance_address: &SocketAddr) {
     if let Some(addrs) = self.instances.get_mut(app_id) {
-      let id = addrs.last().map(|b| (*b.borrow_mut()).id ).unwrap_or(0) + 1;
+      let id = addrs.instances.last().map(|b| (*b.borrow_mut()).id ).unwrap_or(0) + 1;
       let backend = Rc::new(RefCell::new(Backend::new(*instance_address, id)));
-      if !addrs.contains(&backend) {
-        addrs.push(backend);
+      if !addrs.instances.contains(&backend) {
+        addrs.instances.push(backend);
       }
     }
 
     if self.instances.get(app_id).is_none() {
       let backend = Backend::new(*instance_address, 0);
-      self.instances.insert(String::from(app_id), vec![Rc::new(RefCell::new(backend))]);
+      self.instances.insert(String::from(app_id), BackendList { instances: vec![Rc::new(RefCell::new(backend))] });
     }
   }
 
   pub fn remove_instance(&mut self, app_id: &str, instance_address: &SocketAddr) {
     if let Some(instances) = self.instances.get_mut(app_id) {
-      instances.retain(|backend| &(*backend.borrow()).address != instance_address);
+      instances.remove_instance(instance_address);
     } else {
       error!("Instance was already removed");
     }
@@ -43,7 +43,7 @@ impl BackendMap {
 
   pub fn close_backend_connection(&mut self, app_id: &str, addr: &SocketAddr) {
     if let Some(app_instances) = self.instances.get_mut(app_id) {
-      if let Some(ref mut backend) = app_instances.iter_mut().find(|backend| &(*backend.borrow()).address == addr) {
+      if let Some(ref mut backend) = app_instances.find_instance(addr) {
         (*backend.borrow_mut()).dec_connections();
       }
     }
@@ -51,7 +51,7 @@ impl BackendMap {
 
   pub fn backend_from_app_id(&mut self, app_id: &str) -> Result<(Rc<RefCell<Backend>>,TcpStream),ConnectionError> {
     if let Some(ref mut app_instances) = self.instances.get_mut(app_id) {
-      if app_instances.len() == 0 {
+      if app_instances.instances.len() == 0 {
         return Err(ConnectionError::NoBackendAvailable);
       }
 
@@ -61,7 +61,7 @@ impl BackendMap {
 
       for _ in 0..max_failures {
         //FIXME: it's probably pretty wasteful to refilter every time here
-        let mut instances:Vec<&mut Rc<RefCell<Backend>>> = app_instances.iter_mut().filter(|backend| (*backend.borrow()).can_open(max_failures_per_backend)).collect();
+        let mut instances:Vec<&mut Rc<RefCell<Backend>>> = app_instances.instances.iter_mut().filter(|backend| (*backend.borrow()).can_open(max_failures_per_backend)).collect();
         if instances.is_empty() {
           error!("no more available backends for app {}", app_id);
           return Err(ConnectionError::NoBackendAvailable);
@@ -94,7 +94,7 @@ impl BackendMap {
     let max_failures_per_backend = 10;
 
     let sticky_conn: Option<Result<(Rc<RefCell<Backend>>,TcpStream),ConnectionError>> = self.instances.get_mut(app_id).and_then(|app_instances| {
-      app_instances.iter_mut().find(|b| {
+      app_instances.instances.iter_mut().find(|b| {
         let backend = &*b.borrow();
         backend.id == sticky_session && backend.can_open(max_failures_per_backend)
       })
@@ -116,5 +116,19 @@ impl BackendMap {
       debug!("Couldn't find a backend corresponding to sticky_session {} for app {}", sticky_session, app_id);
       return self.backend_from_app_id(app_id);
     }
+  }
+}
+
+pub struct BackendList {
+  pub instances: Vec<Rc<RefCell<Backend>>>,
+}
+
+impl BackendList {
+  pub fn remove_instance(&mut self, instance_address: &SocketAddr) {
+    self.instances.retain(|backend| &(*backend.borrow()).address != instance_address);
+  }
+
+  pub fn find_instance(&mut self, instance_address: &SocketAddr) -> Option<&mut Rc<RefCell<Backend>>> {
+    self.instances.iter_mut().find(|backend| &(*backend.borrow()).address == instance_address)
   }
 }
