63a1ffa36cc19e286f9ce2130d140a9d77131402
Geoffroy Couprie <geo.couprie@gmail.com>
2017-11-21 17:26:52 +0100

    make the examples slightly more readable

--- lib/examples/main.rs
+++ lib/examples/main.rs
@@ -43,10 +43,29 @@ fn main() {
     network::http::start(config, channel);
   });
 
-  let http_front = messages::HttpFront { app_id: String::from("app_1"), hostname: String::from("lolcatho.st"), path_begin: String::from("/") };
-  let http_instance = messages::Instance { app_id: String::from("app_1"),  instance_id: String::from("app_1-0"), ip_address: String::from("127.0.0.1"), port: 1026 };
-  command.write_message(&messages::OrderMessage { id: String::from("ID_ABCD"), order: messages::Order::AddHttpFront(http_front) });
-  command.write_message(&messages::OrderMessage { id: String::from("ID_EFGH"), order: messages::Order::AddInstance(http_instance) });
+  let http_front = messages::HttpFront {
+    app_id:     String::from("app_1"),
+    hostname:   String::from("lolcatho.st"),
+    path_begin: String::from("/")
+  };
+
+  let http_instance = messages::Instance {
+    app_id:      String::from("app_1"),
+    instance_id: String::from("app_1-0"),
+    ip_address:  String::from("127.0.0.1"),
+    port:        1026
+  };
+
+  command.write_message(&messages::OrderMessage {
+    id:    String::from("ID_ABCD"),
+    order: messages::Order::AddHttpFront(http_front)
+  });
+
+  command.write_message(&messages::OrderMessage {
+    id:    String::from("ID_EFGH"),
+    order: messages::Order::AddInstance(http_instance)
+  });
+
   info!("MAIN\tHTTP -> {:?}", command.read_message());
   info!("MAIN\tHTTP -> {:?}", command.read_message());
 
@@ -99,9 +118,22 @@ fn main() {
     path_begin:  String::from("/"),
     fingerprint: messages::CertFingerprint(hex::FromHex::from_hex("AB2618B674E15243FD02A5618C66509E4840BA60E7D64CEBEC84CDBFECEEE0C5").unwrap()),
   };
-  command2.write_message(&messages::OrderMessage { id: String::from("ID_IJKL2"), order: messages::Order::AddHttpsFront(tls_front) });
-  let tls_instance = messages::Instance { app_id: String::from("app_1"),  instance_id: String::from("app_1-0"), ip_address: String::from("127.0.0.1"), port: 1026 };
-  command2.write_message(&messages::OrderMessage { id: String::from("ID_MNOP"), order: messages::Order::AddInstance(tls_instance) });
+
+  command2.write_message(&messages::OrderMessage {
+    id:    String::from("ID_IJKL2"),
+    order: messages::Order::AddHttpsFront(tls_front)
+  });
+  let tls_instance = messages::Instance {
+    app_id:      String::from("app_1"),
+    instance_id: String::from("app_1-0"),
+    ip_address:  String::from("127.0.0.1"),
+    port:        1026
+  };
+
+  command2.write_message(&messages::OrderMessage {
+    id:    String::from("ID_MNOP"),
+    order: messages::Order::AddInstance(tls_instance)
+  });
 
   let cert2 = include_str!("../assets/cert_test.pem");
   let key2  = include_str!("../assets/key_test.pem");
@@ -111,19 +143,35 @@ fn main() {
     key: String::from(key2),
     certificate_chain: vec!()
   };
+
   command2.write_message(&messages::OrderMessage {
     id:    String::from("ID_QRST1"),
     order: messages::Order::AddCertificate(certificate_and_key2)
   });
+
   let tls_front2 = messages::HttpsFront {
     app_id:      String::from("app_2"),
     hostname:    String::from("test.local"),
     path_begin:  String::from("/"),
     fingerprint: messages::CertFingerprint(hex::FromHex::from_hex("7E8EBF9AD0645AB755A2E51EB3734B91D4ACACEF1F28AD9D96D9385487FAE6E6").unwrap()),
   };
-  command2.write_message(&messages::OrderMessage { id: String::from("ID_QRST2"), order: messages::Order::AddHttpsFront(tls_front2) });
-  let tls_instance2 = messages::Instance { app_id: String::from("app_2"), instance_id: String::from("app_2-0"), ip_address: String::from("127.0.0.1"), port: 1026 };
-  command2.write_message(&messages::OrderMessage { id: String::from("ID_UVWX"), order: messages::Order::AddInstance(tls_instance2) });
+
+  command2.write_message(&messages::OrderMessage {
+    id:    String::from("ID_QRST2"),
+    order: messages::Order::AddHttpsFront(tls_front2)
+  });
+
+  let tls_instance2 = messages::Instance {
+    app_id:      String::from("app_2"),
+    instance_id: String::from("app_2-0"),
+    ip_address:  String::from("127.0.0.1"),
+    port:        1026
+  };
+
+  command2.write_message(&messages::OrderMessage {
+    id:    String::from("ID_UVWX"),
+    order: messages::Order::AddInstance(tls_instance2)
+  });
 
   info!("MAIN\tTLS -> {:?}", command2.read_message());
   info!("MAIN\tTLS -> {:?}", command2.read_message());
