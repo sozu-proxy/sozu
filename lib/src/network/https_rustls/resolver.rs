159fcb5dfedd0f3f4ae9d5ec074c3bdd2f0b1bf2
Geoffroy Couprie <geo.couprie@gmail.com>
2017-12-14 14:22:16 +0100

    make a CertifiedKey from PEM certificates

--- lib/src/network/https_rustls/resolver.rs
+++ lib/src/network/https_rustls/resolver.rs
@@ -1,7 +1,9 @@
-use std::sync::Mutex;
+use std::sync::{Arc,Mutex};
 use std::collections::HashMap;
-use rustls::{ResolvesServerCert, SignatureScheme};
-use rustls::sign::CertifiedKey;
+use std::io::BufReader;
+use rustls::{ResolvesServerCert, SignatureScheme, PrivateKey};
+use rustls::sign::{CertifiedKey, RSASigningKey};
+use rustls::internal::pemfile;
 use webpki::DNSNameRef;
 
 use sozu_command::messages::{CertificateAndKey, CertFingerprint};
@@ -21,6 +23,43 @@ impl CertificateResolver {
   }
 
   pub fn add_certificate(&self, certificate_and_key: CertificateAndKey) -> bool {
+
+    let mut chain = Vec::new();
+
+    let mut cert_reader = BufReader::new(certificate_and_key.certificate.as_bytes());
+    let parsed_certs = pemfile::certs(&mut cert_reader);
+    //info!("parsed: {:?}", parsed_cert);
+    if let Ok(certs) = parsed_certs {
+      for cert in certs {
+        chain.push(cert);
+      }
+    } else {
+      return false;
+    }
+
+    for ref cert in certificate_and_key.certificate_chain.iter() {
+      let mut chain_cert_reader = BufReader::new(cert.as_bytes());
+      if let Ok(parsed_chain_certs) = pemfile::certs(&mut chain_cert_reader) {
+        for cert in parsed_chain_certs {
+          chain.push(cert);
+        }
+      }
+    }
+
+    let mut key_reader = BufReader::new(certificate_and_key.key.as_bytes());
+    let parsed_key = pemfile::pkcs8_private_keys(&mut key_reader);
+
+    if let Ok(keys) = parsed_key {
+      if !keys.is_empty() {
+        if let Ok(signing_key) = RSASigningKey::new(&keys[0]) {
+          let certified = CertifiedKey::new(chain, Arc::new(Box::new(signing_key)));
+
+          // add to certificate list here
+
+          return true;
+        }
+      }
+    }
 
     false
   }
