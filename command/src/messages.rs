4b707fb50edae421a96197c55860f368f48793a0
Geoffroy Couprie <geo.couprie@gmail.com>
2017-12-26 18:56:04 +0100

    remove unneeded spaces

--- command/src/messages.rs
+++ command/src/messages.rs
@@ -62,7 +62,7 @@ pub struct FilteredTimeSerie {
 
 impl fmt::Debug for FilteredTimeSerie {
   fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-    write!(f, "FilteredTimeSerie {{\nlast_second: {},\n last_minute:\n{:?}\n{:?}\n{:?}\n{:?}\n{:?}\n{:?}\n last_hour:\n{:?}\n{:?}\n{:?}\n{:?}\n{:?}\n{:?}\n}}",
+    write!(f, "FilteredTimeSerie {{\nlast_second: {},\nlast_minute:\n{:?}\n{:?}\n{:?}\n{:?}\n{:?}\n{:?}\nlast_hour:\n{:?}\n{:?}\n{:?}\n{:?}\n{:?}\n{:?}\n}}",
       self.last_second,
     &self.last_minute[0..10], &self.last_minute[10..20], &self.last_minute[20..30], &self.last_minute[30..40], &self.last_minute[40..50], &self.last_minute[50..60],
     &self.last_hour[0..10], &self.last_hour[10..20], &self.last_hour[20..30], &self.last_hour[30..40], &self.last_hour[40..50], &self.last_hour[50..60])
