sozu-proxy/sozu

commit 2ceae33f7b684528044081a51425ffa84461babb

get the app_id from the Client struct

lib/src/network/https_openssl.rs
@@ -76,6 +76,7 @@ pub struct TlsClient {
   pool:           Weak<RefCell<Pool<BufferQueue>>>,
   sticky_session: bool,
   metrics:        SessionMetrics,
+  pub app_id:     Vec<String>,
 }
 
 impl TlsClient {
@@ -91,6 +92,7 @@ impl TlsClient {
       pool:           pool,
       sticky_session: false,
       metrics:        SessionMetrics::new(),
+      app_id:         None,
     };
     client.readiness().front_interest = UnixReady::from(Ready::readable()) | UnixReady::hup() | UnixReady::error();
 
@@ -300,7 +302,8 @@ impl TlsClient {
   }
 
   fn remove_backend(&mut self) -> (Option<String>, Option<SocketAddr>) {
-    self.http().map(|http| http.remove_backend()).unwrap_or((None, None))
+    let addr:Option<SocketAddr> = self.back_socket().and_then(|sock| sock.peer_addr().ok());
+    (self.app_id.clone(), addr)
   }
 
   fn readiness(&mut self)      -> &mut Readiness {
@@ -1128,6 +1131,7 @@ impl ProxyConfiguration<TlsClient> for ServerConfiguration {
       }
 
       let old_app_id = client.http().and_then(|ref http| http.app_id.clone());
+      client.app_id = Some(app_id.clone());
 
       let conn   = try!(unwrap_msg!(client.http()).state().get_front_keep_alive().ok_or(ConnectionError::ToBeDefined));
       let sticky_session = client.http().unwrap().state.as_ref().unwrap().get_request_sticky_session();

lib/src/network/http.rs
@@ -60,6 +60,7 @@ pub struct Client {
   pool:           Weak<RefCell<Pool<BufferQueue>>>,
   sticky_session: bool,
   metrics:        SessionMetrics,
+  pub app_id:     Option<String>,
 }
 
 impl Client {
@@ -84,6 +85,7 @@ impl Client {
         pool:           pool,
         sticky_session: false,
         metrics:        SessionMetrics::new(),
+        app_id:         None,
       };
 
       client.readiness().front_interest = UnixReady::from(Ready::readable()) | UnixReady::hup() | UnixReady::error();
@@ -262,7 +264,7 @@ impl Client {
       self.backend_token.map(|t| format!("{}", t.0)).unwrap_or("-".to_string()));
     let addr:Option<SocketAddr> = self.back_socket().and_then(|sock| sock.peer_addr().ok());
     self.backend_token = None;
-    (unwrap_msg!(self.http()).app_id.clone(), addr)
+    (self.app_id.clone(), addr)
   }
 
   fn readiness(&mut self) -> &mut Readiness {
@@ -755,6 +757,7 @@ impl ProxyConfiguration<Client> for ServerConfiguration {
       }
 
       let old_app_id = client.http().and_then(|ref http| http.app_id.clone());
+      client.app_id = Some(app_id.clone());
 
       let conn = match (front_should_stick, sticky_session) {
         (true, Some(session)) => self.backend_from_sticky_session(client, &app_id, session),

lib/src/network/https_rustls/client.rs
@@ -59,6 +59,7 @@ pub struct TlsClient {
   pool:               Weak<RefCell<Pool<BufferQueue>>>,
   pub sticky_session: bool,
   pub metrics:        SessionMetrics,
+  pub app_id:         Option<String>,
 }
 
 impl TlsClient {
@@ -74,6 +75,7 @@ impl TlsClient {
       pool:           pool,
       sticky_session: false,
       metrics:        SessionMetrics::new(),
+      app_id:         None,
     };
     client.readiness().front_interest = UnixReady::from(Ready::readable()) | UnixReady::hup() | UnixReady::error();
     client
@@ -286,7 +288,8 @@ impl TlsClient {
   }
 
   fn remove_backend(&mut self) -> (Option<String>, Option<SocketAddr>) {
-    unwrap_msg!(self.http()).remove_backend()
+    let addr:Option<SocketAddr> = self.back_socket().and_then(|sock| sock.peer_addr().ok());
+    (self.app_id.clone(), addr)
   }
 
   pub fn readiness(&mut self)      -> &mut Readiness {

lib/src/network/https_rustls/configuration.rs
@@ -420,6 +420,7 @@ impl ProxyConfiguration<TlsClient> for ServerConfiguration {
       }
 
       let old_app_id = client.http().and_then(|ref http| http.app_id.clone());
+      client.app_id = Some(app_id.clone());
 
       let conn   = try!(unwrap_msg!(client.http()).state().get_front_keep_alive().ok_or(ConnectionError::ToBeDefined));
       let sticky_session = client.http().unwrap().state.as_ref().unwrap().get_request_sticky_session();

commit 0d7d85ae6c031659e4421c53ae95eb4c0e780bc0

https_openssl: fix TlsClient::app_id type typo
Introduced in 2ceae33f7b684528044081a51425ffa84461babb

lib/src/network/https_openssl.rs
@@ -76,7 +76,7 @@ pub struct TlsClient {
   pool:           Weak<RefCell<Pool<BufferQueue>>>,
   sticky_session: bool,
   metrics:        SessionMetrics,
-  pub app_id:     Vec<String>,
+  pub app_id:     Option<String>,
 }
 
 impl TlsClient {