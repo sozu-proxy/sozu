c3475c7c2cde36c3d38570cb407e79ebb3276a64
Alessio Coltellacci <lightplay8@gmail.com>
2018-03-13 17:20:34 +0100

    Refactor the proxy protocol parser following the review

--- lib/src/parser/proxy_protocol.rs
+++ lib/src/parser/proxy_protocol.rs
@@ -1,6 +1,4 @@
-use nom::IResult;
-use nom::IResult::*;
-use nom::{be_u8, be_u16, ErrorKind};
+use nom::{be_u8, be_u16};
 
 use std::net::{Ipv4Addr, Ipv6Addr, SocketAddrV4, SocketAddrV6};
 use std::convert::AsMut;
@@ -8,44 +6,13 @@ use std::convert::From;
 
 use network::protocol::proxy_protocol::header::*;
 
-pub const UNKNOWN_FAMILY: u32 = 1;
-pub const INCORRECT_VERSION: u32 = 2;
-
 const PROTOCOL_SIGNATURE_V2: [u8; 12] = [0x0D, 0x0A, 0x0D, 0x0A, 0x00, 0x0D, 0x0A, 0x51, 0x55, 0x49, 0x54, 0x0A];
 
-//NOTE: Only support the V2 for the moment
-pub fn parse(buf: &[u8]) -> IResult<&[u8], ProxyProtocolHeader> {
-  match tag!(buf, &PROTOCOL_SIGNATURE_V2) {
-    Done(rest, _) => {
-      match parse_v2_header(rest) {
-        Done(rest, header) => Done(rest, ProxyProtocolHeader::V2(header)),
-        Incomplete(i) => Incomplete(i),
-        Error(e) => Error(e),
-      }
-    },
-    Incomplete(i) => Incomplete(i),
-    Error(e) => Error(e),
-  }
-}
+named!(read_and_verify_ver_and_command<u8>, verify!(be_u8, |ver_and_cmd: u8| (ver_and_cmd >> 4) & 0x0f == 0x02));
 
-//TODO: Refactor this method using cond! parser if it's possible
-fn read_and_verify_ver_and_command(input: &[u8]) -> IResult<&[u8], u8> {
-  match be_u8(input) {
-    Done(rest, ver_and_cmd) => {
-      // Verify if it's the version 2
-      if (ver_and_cmd >> 4) & 0x0f == 0x02 {
-        return Done(rest, ver_and_cmd)
-      }
-      Error(error_code!(ErrorKind::Custom(INCORRECT_VERSION)))
-    },
-    Error(e)      => Error(e),
-    Incomplete(e) => Incomplete(e)
-  }
-}
-
-fn parse_v2_header(input: &[u8]) -> IResult<&[u8], HeaderV2> {
+named!(pub parse_v2_header<HeaderV2>,
   do_parse!(
-    input,
+    signature: tag!(&PROTOCOL_SIGNATURE_V2) >>
     ver_and_cmd: read_and_verify_ver_and_command >>
     family: be_u8 >>
     len: be_u16 >>
@@ -60,24 +27,25 @@ fn parse_v2_header(input: &[u8]) -> IResult<&[u8], HeaderV2> {
       }
     )
   )
-}
+);
 
-fn parse_addr_v2(input: &[u8], family: u8) -> IResult<&[u8], ProxyAddr> {
+named_args!(address_family(family: u8)<u8>, value!((family >> 4) & 0x0f));
+
+named!(unspec<ProxyAddr>, value!(ProxyAddr::AfUnspec));
+
+named_args!(parse_addr_v2(family: u8)<ProxyAddr>,
   // family: 4 bits for address family and 4 bits for transport protocol
-  match (family >> 4) & 0x0f {
-    0x00 => Done(input, ProxyAddr::AfUnspec),
-    0x01 => parse_ipv4_on_v2(input),
-    0x02 => parse_ipv6_on_v2(input),
-    _ => Error(error_code!(ErrorKind::Custom(UNKNOWN_FAMILY))),
-  }
-}
+  switch!(apply!(address_family, family),
+    0x00 => call!(unspec) |
+    0x01 => call!(parse_ipv4_on_v2) |
+    0x02 => call!(parse_ipv6_on_v2)
+  )
+);
 
-//TODO: Maybe we can refactor: e.g. count_fixed!(u8, take!(2), 3)
 named!(take_4_bytes, take!(4));
 
-fn parse_ipv4_on_v2(input: &[u8]) -> IResult<&[u8], ProxyAddr> {
+named!(parse_ipv4_on_v2<ProxyAddr>,
   do_parse!(
-    input,
     src_ip: take_4_bytes >>
     dest_ip: take_4_bytes >>
     src_port: be_u16 >>
@@ -89,20 +57,12 @@ fn parse_ipv4_on_v2(input: &[u8]) -> IResult<&[u8], ProxyAddr> {
       }
     )
   )
-}
+);
 
 named!(take_16_bytes, take!(16));
 
-// Transform a slice to an array
-fn clone_into_array<A, T>(slice: &[T]) -> A where A: Sized + Default + AsMut<[T]>, T: Clone {
-  let mut a = Default::default();
-  <A as AsMut<[T]>>::as_mut(&mut a).clone_from_slice(slice);
-  a
-}
-
-fn parse_ipv6_on_v2(input: &[u8]) -> IResult<&[u8], ProxyAddr> {
+named!(parse_ipv6_on_v2<ProxyAddr>,
   do_parse!(
-    input,
     src_ip: take_16_bytes >>
     dest_ip: take_16_bytes >>
     src_port: be_u16 >>
@@ -114,12 +74,23 @@ fn parse_ipv6_on_v2(input: &[u8]) -> IResult<&[u8], ProxyAddr> {
       }
     )
   )
+);
+
+// Transform a slice to an array
+fn clone_into_array<A, T>(slice: &[T]) -> A where A: Sized + Default + AsMut<[T]>, T: Clone {
+  let mut a = Default::default();
+  <A as AsMut<[T]>>::as_mut(&mut a).clone_from_slice(slice);
+  a
 }
+
 
 #[cfg(test)]
 mod test {
 
   use super::*;
+  use nom::ErrorKind;
+  use nom::IResult;
+  use nom::IResult::*;
   use std::net::{IpAddr, SocketAddr};
   use nom::Needed::Size;
 
@@ -138,9 +109,9 @@ mod test {
 
     let src_addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(125, 25, 10, 1)), 8080);
     let dst_addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(10, 4, 5, 8)), 4200);
-    let expected = ProxyProtocolHeader::V2(HeaderV2::new(Some((src_addr, dst_addr))));
+    let expected = HeaderV2::new(Some((src_addr, dst_addr)));
 
-    assert_eq!(Done(&[][..], expected), parse(input));
+    assert_eq!(Done(&[][..], expected), parse_v2_header(input));
   }
 
   #[test]
@@ -158,9 +129,9 @@ mod test {
 
     let src_addr = SocketAddr::new(IpAddr::V6(Ipv6Addr::new(0, 0, 0, 0, 0, 0, 0, 1)), 8080);
     let dst_addr = SocketAddr::new(IpAddr::V6(Ipv6Addr::new(0, 0, 0, 0, 0, 0, 0, 2)), 4200);
-    let expected = ProxyProtocolHeader::V2(HeaderV2::new(Some((src_addr, dst_addr))));
+    let expected = HeaderV2::new(Some((src_addr, dst_addr)));
 
-    assert_eq!(Done(&[][..], expected), parse(input));
+    assert_eq!(Done(&[][..], expected), parse_v2_header(input));
   }
 
   #[test]
@@ -172,9 +143,9 @@ mod test {
       0x00, 0x00,                                                             // address sizes = 0
     ];
 
-    let expected = ProxyProtocolHeader::V2(HeaderV2::new(None));
+    let expected = HeaderV2::new(None);
 
-    assert_eq!(Done(&[][..], expected), parse(input));
+    assert_eq!(Done(&[][..], expected), parse_v2_header(input));
   }
 
   #[test]
@@ -186,7 +157,7 @@ mod test {
       unknow_version,                                                         // Version 2 and command LOCAL
     ];
 
-    assert_eq!(Error(ErrorKind::Custom(INCORRECT_VERSION)), parse(input));
+    assert_eq!(Error(ErrorKind::Verify), parse_v2_header(input));
   }
 
   #[test]
@@ -200,7 +171,7 @@ mod test {
       0x00, 0x00,                                                             // address sizes = 0
     ];
 
-    assert_eq!(Error(ErrorKind::Custom(UNKNOWN_FAMILY)), parse(input));
+    assert_eq!(Error(ErrorKind::Switch), parse_v2_header(input));
   }
 
   #[test]
@@ -210,7 +181,7 @@ mod test {
       0x0D, 0x0D, 0x0D, 0x0D, 0x0D, 0x0D, 0x0D, 0x0D, 0x0D, 0x0D, 0x0D, 0x0D, // INCORRECT MAGIC header
     ];
 
-    assert_eq!(Error(ErrorKind::Tag), parse(input));
+    assert_eq!(Error(ErrorKind::Tag), parse_v2_header(input));
   }
 
   #[test]
@@ -221,6 +192,6 @@ mod test {
       0x11,                                                                   // family AF_UNIX with IPv4
     ];
 
-    assert_eq!(Incomplete(Size(4)), parse(input));
+    assert_eq!(Incomplete(Size(16)), parse_v2_header(input));
   }
 }
