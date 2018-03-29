7adf8dc2f23c69294dc7a15b5aa343e6f6c1f9d0
Geoffroy Couprie <geo.couprie@gmail.com>
2018-03-29 16:01:23 +0200

    fix compilation for the futures example

--- futures/examples/main.rs
+++ futures/examples/main.rs
@@ -13,7 +13,7 @@ use command::SozuCommandClient;
 use sozu_command::data::{ConfigCommand,ConfigMessage};
 
 fn main() {
-    env_logger::init().unwrap();
+    env_logger::init();
     let mut core   = Core::new().unwrap();
     let handle     = core.handle();
 
