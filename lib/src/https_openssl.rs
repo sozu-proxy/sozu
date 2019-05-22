b50e63a54566cd8b5169b4407cf85140269a8940
Geoffroy Couprie <geo.couprie@gmail.com>
2019-05-22 16:11:58 +0200

    really fix the build this time

--- lib/src/https_openssl.rs
+++ lib/src/https_openssl.rs
@@ -1541,10 +1541,12 @@ impl ProxyConfiguration<Session> for Proxy {
       },
       ProxyRequestData::Query(Query::Certificates(QueryCertificateType::All)) => {
         let res = self.listeners.iter().map(|(addr, listener)| {
-          let domains  = unwrap_msg!(listener.domains.lock()).to_hashmap();
-          (listener.address, domains.drain().map(|(k, v)| {
+          let mut domains = unwrap_msg!(listener.domains.lock()).to_hashmap();
+          let res = domains.drain().map(|(k, v)| {
             (String::from_utf8(k).unwrap(), v.0.clone())
-          }).collect())
+          }).collect();
+
+          (listener.address, res)
         }).collect::<HashMap<_,_>>();
 
         info!("got Certificates::All query, answering with {:?}", res);
