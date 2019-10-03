6238c0cf2ec5a3b0d7fda3c78ff2784a776bc285
Geoffroy Couprie <geo.couprie@gmail.com>
2019-10-03 14:41:36 +0200

    update to cookie-factory 0.3

--- lib/src/protocol/h2/serializer.rs
+++ lib/src/protocol/h2/serializer.rs
@@ -1,16 +1,15 @@
-use cookie_factory::GenError;
+use cookie_factory::{GenError, gen, bytes::{be_u8, be_u24, be_u32}, sequence::tuple};
 use super::parser::{FrameHeader,FrameType};
 
 pub fn gen_frame_header<'a, 'b>(x: (&'a mut [u8], usize), frame: &'b FrameHeader) -> Result<(&'a mut [u8], usize), GenError> {
-  /*
-  do_gen!(x,
-    gen_be_u24!(frame.payload_len) >>
-    gen_be_u8!(serialize_frame_type(&frame.frame_type)) >>
-    gen_be_u8!(frame.flags) >>
-    gen_be_u32!(frame.stream_id)
-  )
-    */
-  unimplemented!()
+  let serializer = tuple((
+    be_u24(frame.payload_len),
+    be_u8(serialize_frame_type(&frame.frame_type)),
+    be_u8(frame.flags),
+    be_u32(frame.stream_id)
+  ));
+
+  gen(serializer, x.0).map(|(buf, sz)| (buf, sz as usize))
 }
 
 pub fn serialize_frame_type(f: &FrameType) -> u8 {
