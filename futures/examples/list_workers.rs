1200908468a1e80e7ae2f3501f70ce1c5b63bbfb
Geoffroy Couprie <contact@geoffroycouprie.com>
2021-07-08 11:30:09 +0200

    update the futures library

--- futures/examples/list_workers.rs
+++ futures/examples/list_workers.rs
@@ -1,36 +1,28 @@
-#[macro_use] extern crate log;
-extern crate futures;
-extern crate tokio_uds;
-extern crate tokio_core;
-extern crate env_logger;
-extern crate sozu_command_lib as sozu_command;
+#[macro_use]
+extern crate log;
 extern crate sozu_command_futures as command;
+extern crate sozu_command_lib as sozu_command;
 
-use futures::future::Future;
-use tokio_uds::UnixStream;
-use tokio_core::reactor::Core;
 use command::SozuCommandClient;
-use sozu_command::command::{CommandRequestData,CommandRequest};
+use sozu_command::command::{CommandRequest, CommandRequestData};
+use tokio::net::UnixStream;
 
-fn main() {
+#[tokio::main]
+pub async fn main() -> Result<(), std::io::Error> {
     env_logger::init();
-    let mut core   = Core::new().unwrap();
 
-    core.run(
-        Box::new(futures::future::lazy( || {
-            UnixStream::connect("/Users/geal/dev/rust/projects/yxorp/bin/sock").and_then(|stream| {
-                let mut client = SozuCommandClient::new(stream);
-                client.send(CommandRequest::new(
-                    String::from("message-id-42"),
-                    CommandRequestData::ListWorkers,
-                    None,
-                )).and_then(|answer| {
-                    info!("received answer: {:?}", answer);
-                    Ok(())
-                })
-            })
-        }))
+    let conn = UnixStream::connect("/path/to/sozu.sock").await?;
+    let mut client = SozuCommandClient::new(conn);
 
-    ).unwrap();
+    let response = client
+        .send(CommandRequest::new(
+            String::from("message-id-42"),
+            CommandRequestData::ListWorkers,
+            None,
+        ))
+        .await;
+
+    info!("got an answer: {:?}", response);
+
+    Ok(())
 }
-
--- futures/src/lib.rs
+++ futures/src/lib.rs
@@ -1,27 +1,19 @@
-#[macro_use] extern crate log;
-extern crate futures;
-extern crate tokio_codec;
-extern crate tokio_uds;
-extern crate bytes;
-extern crate serde;
-
-extern crate serde_json;
+#[macro_use]
+extern crate log;
 extern crate sozu_command_lib as sozu_command;
 
 use bytes::BytesMut;
+use futures::{SinkExt, TryStreamExt};
+use sozu_command::command::{CommandRequest, CommandResponse, CommandStatus};
 use std::io::{self, Error, ErrorKind};
-use std::sync::{Arc,Mutex};
-use std::collections::hash_map::HashMap;
-use futures::{Async, Sink, Stream, Future, future};
-use tokio_uds::UnixStream;
-use tokio_codec::{Decoder, Encoder, Framed};
 use std::str::from_utf8;
-use sozu_command::command::{CommandRequest,CommandResponse,CommandStatus};
+use tokio::net::UnixStream;
+use tokio_util::codec::{Decoder, Encoder, Framed};
 
 pub struct CommandCodec;
 
 impl Decoder for CommandCodec {
-    type Item  = CommandResponse;
+    type Item = CommandResponse;
     type Error = io::Error;
 
     fn decode(&mut self, buf: &mut BytesMut) -> Result<Option<CommandResponse>, io::Error> {
@@ -29,16 +21,20 @@ impl Decoder for CommandCodec {
             let res = if let Ok(s) = from_utf8(&buf[..pos]) {
                 match serde_json::from_str(s) {
                     Ok(message) => Ok(Some(message)),
-                    Err(e) => {
-                        Err(io::Error::new(io::ErrorKind::Other, format!("parse error: {:?}", e)))
-                    }
+                    Err(e) => Err(io::Error::new(
+                        io::ErrorKind::Other,
+                        format!("parse error: {:?}", e),
+                    )),
                 }
             } else {
-                Err(io::Error::new(io::ErrorKind::InvalidData, String::from("could not parse UTF-8 data")))
+                Err(io::Error::new(
+                    io::ErrorKind::InvalidData,
+                    String::from("could not parse UTF-8 data"),
+                ))
             };
 
             if pos < buf.len() {
-              buf.split_to(pos+1);
+                let _ = buf.split_to(pos + 1);
             }
 
             res
@@ -48,121 +44,61 @@ impl Decoder for CommandCodec {
     }
 }
 
-impl Encoder for CommandCodec {
-    type Item = CommandRequest;
+impl Encoder<CommandRequest> for CommandCodec {
     type Error = io::Error;
 
     fn encode(&mut self, message: CommandRequest, buf: &mut BytesMut) -> Result<(), Self::Error> {
         match serde_json::to_string(&message) {
-        Ok(data) => {
-            trace!("encoded message: {}", data);
-            buf.extend(data.as_bytes());
-            buf.extend(&[0u8][..]);
-            trace!("buffer content: {:?}", from_utf8(&buf[..]));
-            Ok(()) },
-        Err(e) => Err(io::Error::new(io::ErrorKind::Other, format!("serialization error: {:?}", e)))
+            Ok(data) => {
+                trace!("encoded message: {}", data);
+                buf.extend(data.as_bytes());
+                buf.extend(&[0u8][..]);
+                trace!("buffer content: {:?}", from_utf8(&buf[..]));
+                Ok(())
+            }
+            Err(e) => Err(io::Error::new(
+                io::ErrorKind::Other,
+                format!("serialization error: {:?}", e),
+            )),
         }
     }
 }
 
-pub struct SozuCommandTransport {
-    upstream: Framed<UnixStream,CommandCodec>,
-    received: HashMap<String,CommandResponse>,
-}
-
-impl SozuCommandTransport {
-    pub fn new(stream: UnixStream) -> SozuCommandTransport {
-        SozuCommandTransport {
-            upstream: CommandCodec.framed(stream),
-            received: HashMap::new(),
-        }
-    }
-}
-
-#[derive(Clone)]
 pub struct SozuCommandClient {
-    transport: Arc<Mutex<SozuCommandTransport>>,
+    transport: Framed<UnixStream, CommandCodec>,
 }
 
 impl SozuCommandClient {
     pub fn new(stream: UnixStream) -> SozuCommandClient {
         SozuCommandClient {
-            transport: Arc::new(Mutex::new(SozuCommandTransport::new(stream))),
+            transport: CommandCodec.framed(stream),
         }
     }
 
-    pub fn send(&mut self, message: CommandRequest)  -> Box<Future<Item = CommandResponse, Error = io::Error> + Send + 'static> {
+    pub async fn send(&mut self, message: CommandRequest) -> Result<CommandResponse, io::Error> {
         trace!("will send message: {:?}", message);
-        let tr  = self.transport.clone();
-        let tr2 = self.transport.clone();
 
-        if let Ok(mut transport) = self.transport.lock() {
-            let id = message.id.clone();
-            let res = transport.upstream.start_send(message);
-            trace!("start_send result: {:?}", res);
+        let id = message.id.clone();
+        self.transport.send(message).await?;
 
-            Box::new(future::poll_fn(move || {
-                if let Ok(mut transport) = tr2.try_lock() {
-                    let res = transport.upstream.poll_complete();
-                    res
-                } else {
-                    Err(Error::new(ErrorKind::ConnectionAborted, format!("could not send message")))
-                }
-            }).and_then(|_| {
-                future::poll_fn(move || {
-                    trace!("polling for id = {}", id);
-                    if let Ok(mut transport) = tr.try_lock() {
-                        if let Some(message) = transport.received.remove(&id) {
-                            if message.status == CommandStatus::Processing {
-                                info!("processing: {:?}", message);
-                            } else {
-                                return Ok(Async::Ready(message))
-                            }
-                        }
-
-                        let value = match transport.upstream.poll() {
-                            Ok(Async::Ready(t)) => t,
-                            Ok(Async::NotReady) => {
-                                trace!("upstream poll gave NotReady");
-                                return Ok(Async::NotReady);
-                            },
-                            Err(e) => {
-                                trace!("upstream poll gave error: {:?}", e);
-                                return Err(From::from(e));
-                            },
-                        };
-
-                        if let Some(message) = value {
-                            trace!("upstream poll gave message: {:?}", message);
-                            if message.id != id {
-                                transport.received.insert(message.id.clone(), message);
-                                Ok(Async::NotReady)
-                            } else {
-                                if message.status == CommandStatus::Processing {
-                                    info!("processing: {:?}", message);
-                                    Ok(Async::NotReady)
-                                } else {
-                                    Ok(Async::Ready(message))
-                                }
-                            }
-                        } else {
-                            trace!("upstream poll gave Ready(None)");
-                            Ok(Async::NotReady)
-                        }
-                    } else {
-                        //FIXME: if we're there, it means the mutex failed
-                        Err(
-                            Error::new(ErrorKind::ConnectionAborted, format!("could not send message"))
-                        )
+        loop {
+            match self.transport.try_next().await? {
+                None => {}
+                Some(msg) => {
+                    if msg.id != id {
+                        return Err(Error::new(
+                            ErrorKind::ConnectionAborted,
+                            format!("could not send message"),
+                        ));
                     }
 
-                })
-            }))
-        } else {
-            //FIXME: if we're there, it means the mutex failed
-            Box::new(future::err(
-                Error::new(ErrorKind::ConnectionAborted, format!("could not send message"))
-            ))
+                    if msg.status == CommandStatus::Processing {
+                        info!("processing: {:?}", msg);
+                    } else {
+                        return Ok(msg);
+                    }
+                }
+            }
         }
     }
 }
