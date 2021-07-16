49038b1b70e44b18722bc62060d100d70b1b7a1d
Geoffroy Couprie <contact@geoffroycouprie.com>
2021-07-16 10:59:26 +0200

    decrement websocket active requests in log_request_error too

--- lib/src/protocol/pipe.rs
+++ lib/src/protocol/pipe.rs
@@ -293,6 +293,11 @@ impl<Front:SocketHandler> Pipe<Front> {
     }
 
     let proto = self.protocol_string();
+    match self.protocol {
+      Protocol::TCP  => {},
+      _ => gauge_add!("websocket.active_requests", -1),
+    }
+
 
     error_access!("{}{} -> {}\t{} {} {} {}\t{} {} | {}",
       self.log_ctx, session, backend,
