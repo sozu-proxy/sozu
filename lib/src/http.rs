sozu-proxy/sozu

commit ae8fa32a586425012e9bfe28e963e2186b836b93

handle more precisely when back sockets are closed
we were not accounting correctly for read closed events and back sockets
could linger for a long time while the backend considered them closed.
With this we will better detect (when the platform handles it) closed
sockets

lib/src/http.rs
@@ -571,7 +571,7 @@ impl Session {
       if back_interest.is_hup() {
         let order = self.back_hup();
         match order {
-          SessionResult::CloseSession => {
+          SessionResult::CloseSession | SessionResult::CloseBackend(_) => {
             return order;
           },
           SessionResult::Continue => {

lib/src/https_openssl.rs
@@ -634,7 +634,7 @@ impl Session {
       if back_interest.is_hup() {
         let order = self.back_hup();
         match order {
-          SessionResult::CloseSession => {
+          SessionResult::CloseSession | SessionResult::CloseBackend(_) => {
             return order;
           },
           SessionResult::Continue => {

lib/src/https_rustls/session.rs
@@ -628,7 +628,7 @@ impl Session {
       if back_interest.is_hup() {
         let order = self.back_hup();
         match order {
-          SessionResult::CloseSession => {
+          SessionResult::CloseSession | SessionResult::CloseBackend(_) => {
             return order;
           },
           SessionResult::Continue => {},

command/src/ready.rs
@@ -161,6 +161,9 @@ impl std::convert::From<&mio::event::Event> for Ready {
             r.insert(Ready::error());
         }
         //FIXME: handle HUP events
+        if e.is_read_closed() || e.is_write_closed() {
+            r.insert(Ready::hup());
+        }
 
         r
     }

lib/src/protocol/http/mod.rs
@@ -412,7 +412,13 @@ impl<Front:SocketHandler> Http<Front> {
         SessionResult::Continue
       }
     } else {
-      SessionResult::CloseSession
+
+        if self.request == Some(RequestState::Initial) &&
+            self.response == Some(ResponseState::Initial) {
+                SessionResult::CloseBackend(self.backend_token.clone())
+            } else {
+                SessionResult::Continue
+            }
     }
   }
 

commit 4250a87f784442653f5781a9dd264ddaeb21df1e

fix the event condition on closed but readable backend sockets
commit ae8fa32a caused some event loop issues by marking sockets as
closed (correctly) while they still hold data, and that data was not
read.

commit f75797af tried to fix that condition by looking at whether the
socket was readable, but when we fail to connect to the backend server,
we can get in a case where the back socket is marked as readable and
HUP, but test_back_socket() would fail, and the code would not consider
the connection as failed but try to go on and send the request to the
backend, and read from the backend (since it is readable, which would
generate those error messages:
SOCKET	socket_write error=Os { code: 111, kind: ConnectionRefused, message: "Connection refused" }

Those messages were silenced by f2aa77bb but it was not fixing the
underlying issue

This commit properly fixes the event issue, by relying on the result of
test_back_socket()

lib/src/http.rs
@@ -485,9 +485,8 @@ impl Session {
 
       self.http_mut().map(|h| h.cancel_backend_timeout());
 
-      if (self.back_readiness().map(|r| r.event.is_hup()).unwrap_or(false)
-          || !self.http_mut().map(|h| h.test_back_socket()).unwrap_or(false))
-        && !self.back_readiness().map(|r| r.event.is_readable()).unwrap_or(false) {
+      if self.back_readiness().map(|r| r.event.is_hup()).unwrap_or(false)
+          && !self.http_mut().map(|h| h.test_back_socket()).unwrap_or(false) {
 
         //retry connecting the backend
         error!("{} error connecting to backend, trying again", self.log_context());

lib/src/https_openssl.rs
@@ -553,9 +553,8 @@ impl Session {
 
       self.http_mut().map(|h| h.cancel_backend_timeout());
 
-      if (self.back_readiness().map(|r| r.event.is_hup()).unwrap_or(false)
-          || !self.http_mut().map(|h| h.test_back_socket()).unwrap_or(false))
-        && !self.back_readiness().map(|r| r.event.is_readable()).unwrap_or(false) {
+      if self.back_readiness().map(|r| r.event.is_hup()).unwrap_or(false)
+          && !self.http_mut().map(|h| h.test_back_socket()).unwrap_or(false) {
 
         //retry connecting the backend
         error!("{} error connecting to backend, trying again", self.log_context());

lib/src/https_rustls/session.rs
@@ -547,9 +547,8 @@ impl Session {
 
       self.http_mut().map(|h| h.cancel_backend_timeout());
 
-      if (self.back_readiness().map(|r| r.event.is_hup()).unwrap_or(false)
-          || !self.http_mut().map(|h| h.test_back_socket()).unwrap_or(false))
-        && !self.back_readiness().map(|r| r.event.is_readable()).unwrap_or(false) {
+      if self.back_readiness().map(|r| r.event.is_hup()).unwrap_or(false)
+          && !self.http_mut().map(|h| h.test_back_socket()).unwrap_or(false) {
 
         //retry connecting the backend
         error!("{} error connecting to backend, trying again", self.log_context());

lib/src/tcp.rs
@@ -613,7 +613,7 @@ impl ProxySession for Session {
     self.metrics().service_start();
 
     if self.back_connected().is_connecting() {
-      if self.back_readiness().unwrap().event.is_hup() || !self.test_back_socket() {
+      if self.back_readiness().unwrap().event.is_hup() && !self.test_back_socket() {
         //retry connecting the backend
         error!("error connecting to backend, trying again");
         self.metrics().service_stop();