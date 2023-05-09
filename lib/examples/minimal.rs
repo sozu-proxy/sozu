17f7cb621fb93fac11968b7fa4062081a1812965
Emmanuel Bosquet <bjokac@gmail.com>
2023-05-09 11:09:54 +0200

    rewrite the sozu_lib documentation
    rewrite the minimal example

--- lib/examples/minimal.rs
+++ lib/examples/minimal.rs
@@ -1,4 +1,3 @@
-#[macro_use]
 extern crate time;
 
 #[macro_use]
@@ -14,10 +13,11 @@ use sozu_command_lib::{
     info,
     logging::{Logger, LoggerBackend},
     proto::command::{
-        request::RequestType, AddBackend, LoadBalancingParams, PathRule, Request,
-        RequestHttpFrontend, RulePosition,
+        request::RequestType, AddBackend, Cluster, LoadBalancingAlgorithms, LoadBalancingParams,
+        PathRule, Request, RequestHttpFrontend, RulePosition,
     },
     request::WorkerRequest,
+    response::WorkerResponse,
 };
 
 fn main() -> anyhow::Result<()> {
@@ -39,22 +39,35 @@ fn main() -> anyhow::Result<()> {
 
     info!("starting up");
 
-    let http_listener = ListenerBuilder::new_http("127.0.0.1:8080").to_http()?;
+    let http_listener = ListenerBuilder::new_http("127.0.0.1:8080")
+        .to_http()
+        .expect("Could not create HTTP listener");
 
-    let (mut command_channel, proxy_channel) =
-        Channel::generate(1000, 10000).with_context(|| "should create a channel")?;
+    let (mut command_channel, proxy_channel): (
+        Channel<WorkerRequest, WorkerResponse>,
+        Channel<WorkerResponse, WorkerRequest>,
+    ) = Channel::generate(1000, 10000).with_context(|| "should create a channel")?;
 
-    let jg = thread::spawn(move || {
+    let worker_thread_join_handle = thread::spawn(move || {
         let max_buffers = 500;
         let buffer_size = 16384;
         sozu_lib::http::start_http_worker(http_listener, proxy_channel, max_buffers, buffer_size)
             .expect("The worker could not be started, or shut down");
     });
 
+    let cluster = Cluster {
+        cluster_id: "my-cluster".to_string(),
+        sticky_session: false,
+        https_redirect: false,
+        load_balancing: LoadBalancingAlgorithms::RoundRobin as i32,
+        answer_503: Some("A custom forbidden message".to_string()),
+        ..Default::default()
+    };
+
     let http_front = RequestHttpFrontend {
-        cluster_id: Some(String::from("test")),
+        cluster_id: Some("my-cluster".to_string()),
         address: "127.0.0.1:8080".to_string(),
-        hostname: String::from("example.com"),
+        hostname: "example.com".to_string(),
         path: PathRule::prefix(String::from("/")),
         position: RulePosition::Pre.into(),
         tags: BTreeMap::from([
@@ -64,8 +77,8 @@ fn main() -> anyhow::Result<()> {
         ..Default::default()
     };
     let http_backend = AddBackend {
-        cluster_id: String::from("test"),
-        backend_id: String::from("test-0"),
+        cluster_id: "my-cluster".to_string(),
+        backend_id: "test-backend".to_string(),
         address: "127.0.0.1:8000".to_string(),
         load_balancing_parameters: Some(LoadBalancingParams::default()),
         ..Default::default()
@@ -73,7 +86,16 @@ fn main() -> anyhow::Result<()> {
 
     command_channel
         .write_message(&WorkerRequest {
-            id: String::from("ID_ABCD"),
+            id: String::from("add-the-cluster"),
+            content: Request {
+                request_type: Some(RequestType::AddCluster(cluster)),
+            },
+        })
+        .expect("Could not send AddHttpFrontend request");
+
+    command_channel
+        .write_message(&WorkerRequest {
+            id: String::from("add-the-frontend"),
             content: Request {
                 request_type: Some(RequestType::AddHttpFrontend(http_front)),
             },
@@ -82,7 +104,7 @@ fn main() -> anyhow::Result<()> {
 
     command_channel
         .write_message(&WorkerRequest {
-            id: String::from("ID_EFGH"),
+            id: String::from("add-the-backend"),
             content: Request {
                 request_type: Some(RequestType::AddBackend(http_backend)),
             },
@@ -92,7 +114,7 @@ fn main() -> anyhow::Result<()> {
     println!("HTTP -> {:?}", command_channel.read_message());
     println!("HTTP -> {:?}", command_channel.read_message());
 
-    let _ = jg.join();
+    let _ = worker_thread_join_handle.join();
     info!("good bye");
     Ok(())
 }
