4806fdbd1301a4768bdf320a6f5fcdbb01010148
Eloi DEMOLIS <eloi.demolis@clever-cloud.com>
2023-08-14 20:32:33 +0200

    Mux SessionState (test)
    Signed-off-by: Eloi DEMOLIS <eloi.demolis@clever-cloud.com>

---
+++ lib/src/protocol/mux/serializer.rs
@@ -0,0 +1,37 @@
+use cookie_factory::{
+    bytes::{be_u24, be_u32, be_u8},
+    gen,
+    sequence::tuple,
+    GenError,
+};
+
+use super::parser::{FrameHeader, FrameType};
+
+pub fn gen_frame_header<'a, 'b>(
+    buf: &'a mut [u8],
+    frame: &'b FrameHeader,
+) -> Result<(&'a mut [u8], usize), GenError> {
+    let serializer = tuple((
+        be_u24(frame.payload_len),
+        be_u8(serialize_frame_type(&frame.frame_type)),
+        be_u8(frame.flags),
+        be_u32(frame.stream_id),
+    ));
+
+    gen(serializer, buf).map(|(buf, size)| (buf, size as usize))
+}
+
+pub fn serialize_frame_type(f: &FrameType) -> u8 {
+    match *f {
+        FrameType::Data => 0,
+        FrameType::Headers => 1,
+        FrameType::Priority => 2,
+        FrameType::RstStream => 3,
+        FrameType::Settings => 4,
+        FrameType::PushPromise => 5,
+        FrameType::Ping => 6,
+        FrameType::GoAway => 7,
+        FrameType::WindowUpdate => 8,
+        FrameType::Continuation => 9,
+    }
+}
