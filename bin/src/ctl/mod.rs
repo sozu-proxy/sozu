sozu-proxy/sozu

commit 3a4e4fd93d5451c43f822348e933af1894a2e917

pass Vec<WorkerRequests> instead of ConfigState to new worker
new function read_requests_from_file
Now, creating a new worker means writing ConfigState as requests
in a file, and parsing them when starting the new worker.
Previously, writing/reading the ConfigStated into a file
was done with serde_json reader and writer, which was inefficient.

bin/src/worker.rs
@@ -32,7 +32,7 @@ use sozu_command_lib::{
     logging::target_to_backend,
     proto::command::{request::RequestType, Request, RunState, Status, WorkerInfo},
     ready::Ready,
-    request::WorkerRequest,
+    request::{read_requests_from_file, WorkerRequest},
     response::WorkerResponse,
     scm_socket::{Listeners, ScmSocket},
     state::ConfigState,
@@ -216,8 +216,9 @@ pub fn begin_worker_process(
         error!("Could not block the worker-to-main channel: {}", e);
     }
 
-    let configuration_state_file = unsafe { File::from_raw_fd(configuration_state_fd) };
-    let config_state: ConfigState = serde_json::from_reader(configuration_state_file)
+    let mut configuration_state_file = unsafe { File::from_raw_fd(configuration_state_fd) };
+
+    let initial_state = read_requests_from_file(&mut configuration_state_file)
         .with_context(|| "could not parse configuration state data")?;
 
     let worker_config = worker_to_main_channel
@@ -275,7 +276,7 @@ pub fn begin_worker_process(
         worker_to_main_channel,
         worker_to_main_scm_socket,
         worker_config,
-        config_state,
+        initial_state,
         true,
     )
     .with_context(|| "Could not create server from config")?;
@@ -305,8 +306,9 @@ pub fn fork_main_into_worker(
         tempfile().with_context(|| "could not create temporary file for configuration state")?;
     util::disable_close_on_exec(state_file.as_raw_fd())?;
 
-    serde_json::to_writer(&mut state_file, state)
-        .with_context(|| "could not write upgrade data to temporary file")?;
+    state
+        .write_requests_to_file(&mut state_file)
+        .with_context(|| "Could not write state to file")?;
 
     state_file
         .rewind()

e2e/src/sozu/worker.rs
@@ -102,11 +102,19 @@ impl Worker {
             .send_listeners(&listeners)
             .expect("could not send listeners");
 
+        let initial_state = state
+            .generate_requests()
+            .into_iter()
+            .map(|request| WorkerRequest {
+                id: "initial_state".to_string(),
+                content: request,
+            })
+            .collect();
         let server = Server::try_new_from_config(
             cmd_worker_to_main,
             scm_worker_to_main,
             config,
-            state,
+            initial_state,
             false,
         )
         .expect("could not create sozu worker");
@@ -139,7 +147,14 @@ impl Worker {
             .expect("could not send listeners");
 
         let thread_config = config.to_owned();
-        let thread_state = state.to_owned();
+        let initial_state = state
+            .generate_requests()
+            .into_iter()
+            .map(|request| WorkerRequest {
+                id: "initial_state".to_string(),
+                content: request,
+            })
+            .collect();
         let thread_name = name.to_owned();
         let thread_scm_worker_to_main = scm_worker_to_main.to_owned();
 
@@ -157,7 +172,7 @@ impl Worker {
                 cmd_worker_to_main,
                 thread_scm_worker_to_main,
                 thread_config,
-                thread_state,
+                initial_state,
                 false,
             )
             .expect("could not create sozu worker");

command/src/request.rs
@@ -1,11 +1,17 @@
 use std::{
     error,
     fmt::{self, Display},
+    fs::File,
+    io::Read,
     net::SocketAddr,
     str::FromStr,
 };
 
+use nom::{HexDisplay, Offset};
+
 use crate::{
+    buffer::fixed::Buffer,
+    parser::parse_several_requests,
     proto::command::{
         request::RequestType, LoadBalancingAlgorithms, PathRuleKind, Request, RequestHttpFrontend,
         RulePosition,
@@ -19,6 +25,10 @@ pub enum RequestError {
     InvalidSocketAddress { address: String, error: String },
     #[error("invalid value {value} for field '{name}'")]
     InvalidValue { name: String, value: i32 },
+    #[error("Could not read requests from file: {0}")]
+    FileError(std::io::Error),
+    #[error("Could not parse requests: {0}")]
+    ParseError(String),
 }
 
 impl Request {
@@ -121,6 +131,55 @@ impl fmt::Display for WorkerRequest {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "{}-{:?}", self.id, self.content)
     }
+}
+
+pub fn read_requests_from_file(file: &mut File) -> Result<Vec<WorkerRequest>, RequestError> {
+    let mut acc = Vec::new();
+    let mut buffer = Buffer::with_capacity(200000);
+    loop {
+        let previous = buffer.available_data();
+
+        let bytes_read = file
+            .read(buffer.space())
+            .map_err(|e| RequestError::FileError(e))?;
+
+        buffer.fill(bytes_read);
+
+        if buffer.available_data() == 0 {
+            debug!("Empty buffer");
+            break;
+        }
+
+        let mut offset = 0usize;
+        match parse_several_requests::<WorkerRequest>(buffer.data()) {
+            Ok((i, requests)) => {
+                if !i.is_empty() {
+                    debug!("could not parse {} bytes", i.len());
+                    if previous == buffer.available_data() {
+                        break;
+                    }
+                }
+                offset = buffer.data().offset(i);
+
+                acc.push(requests);
+            }
+            Err(nom::Err::Incomplete(_)) => {
+                if buffer.available_data() == buffer.capacity() {
+                    error!(
+                        "message too big, stopping parsing:\n{}",
+                        buffer.data().to_hex(16)
+                    );
+                    break;
+                }
+            }
+            Err(parse_error) => {
+                return Err(RequestError::ParseError(parse_error.to_string()));
+            }
+        }
+        buffer.consume(offset);
+    }
+    let requests = acc.into_iter().flatten().collect();
+    Ok(requests)
 }
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]

lib/src/server.rs
@@ -269,7 +269,7 @@ impl Server {
         worker_to_main_channel: ProxyChannel,
         worker_to_main_scm: ScmSocket,
         config: Config,
-        config_state: ConfigState,
+        initial_state: Vec<WorkerRequest>,
         expects_initial_status: bool,
     ) -> anyhow::Result<Self> {
         let event_loop = Poll::new().with_context(|| "could not create event loop")?;
@@ -331,7 +331,7 @@ impl Server {
             Some(https),
             None,
             server_config,
-            Some(config_state),
+            Some(initial_state),
             expects_initial_status,
         )
     }
@@ -348,7 +348,7 @@ impl Server {
         https: Option<https::HttpsProxy>,
         tcp: Option<tcp::TcpProxy>,
         server_config: ServerConfig,
-        config_state: Option<ConfigState>,
+        initial_state: Option<Vec<WorkerRequest>>,
         expects_initial_status: bool,
     ) -> anyhow::Result<Self> {
         FEATURES.with(|_features| {
@@ -438,16 +438,10 @@ impl Server {
         };
 
         // initialize the worker with the state we got from a file
-        if let Some(state) = config_state {
-            for (counter, request) in state.generate_requests().iter().enumerate() {
-                let id = format!("INIT-{counter}");
-                let worker_request = WorkerRequest {
-                    id,
-                    content: request.to_owned(),
-                };
-
-                trace!("generating initial config request: {:#?}", worker_request);
-                server.notify_proxys(worker_request);
+        if let Some(requests) = initial_state {
+            for request in requests {
+                trace!("generating initial config request: {:#?}", request);
+                server.notify_proxys(request);
             }
 
             // do not send back answers to the initialization messages

commit d864012ad6cdadbf94630c7fd195095968a6b17d

remove main logger
The main logger was not useful and created conflicts.
* Homogenize setup_logging accross the whole codebase
* take RUST_LOG into account everywhere

* fix tag error introduced by 3a4e4fd9
    the line read_requests_from_file called a function
    and a log statement from the workspace sozu_command_lib
    before a Logger was properly initialized, which caused
    the default logger to be used by the worker (error, "SOZU")

Co-Authored-By: Eloi DEMOLIS <eloi.demolis@clever-cloud.com>

bin/Cargo.toml
@@ -31,7 +31,6 @@ futures = "^0.3.28"
 futures-lite = "^1.13.0"
 hex = "^0.4.3"
 jemallocator = { version = "^0.5.4", optional = true }
-lazy_static = "^1.4.0"
 libc = "^0.2.149"
 log = "^0.4.20"
 mio = { version = "^0.8.8", features = ["os-poll", "net"] }

bin/src/ctl/mod.rs
@@ -4,12 +4,13 @@ use anyhow::Context;
 use sozu_command_lib::{
     channel::Channel,
     config::Config,
+    logging::setup_logging_with_config,
     proto::command::{Request, Response},
 };
 
 use crate::{
     cli::{self, *},
-    get_config_file_path, load_configuration, util,
+    get_config_file_path, load_configuration,
 };
 
 mod command;
@@ -27,7 +28,7 @@ pub fn ctl(args: cli::Args) -> anyhow::Result<()> {
     let config_file_path = get_config_file_path(&args)?;
     let config = load_configuration(config_file_path)?;
 
-    util::setup_logging(&config, "CTL");
+    setup_logging_with_config(&config, "CTL");
 
     // If the command is `config check` then exit because if we are here, the configuration is valid
     if let SubCmd::Config {

bin/src/logging.rs
@@ -1,87 +1,0 @@
-use std::{
-    env,
-    sync::{Arc, Mutex},
-};
-
-use sozu_command_lib::logging::{target_to_backend, Logger, LoggerBackend};
-
-lazy_static! {
-    pub static ref MAIN_LOGGER: Arc<Mutex<Logger>> = Arc::new(Mutex::new(Logger::new()));
-    pub static ref TAG: String = {
-        let logger = MAIN_LOGGER.lock().unwrap();
-        logger.tag.clone()
-    };
-}
-
-#[macro_export]
-macro_rules! log {
-    (__inner__ $target:expr, $lvl:expr, $format:expr, $level_tag:expr,
-     [$($transformed_args:ident),*], [$first_ident:ident $(, $other_idents:ident)*], $first_arg:expr $(, $other_args:expr)*) => ({
-      let $first_ident = &$first_arg;
-      log!(__inner__ $target, $lvl, $format, $level_tag, [$($transformed_args,)* $first_ident], [$($other_idents),*] $(, $other_args)*);
-    });
-
-    (__inner__ $target:expr, $lvl:expr, $format:expr, $level_tag:expr,
-     [$($final_args:ident),*], [$($idents:ident),*]) => ({
-      static _META: $crate::sozu_command_lib::logging::Metadata = $crate::sozu_command_lib::logging::Metadata {
-          level:  $lvl,
-          target: module_path!(),
-      };
-      {
-        let mut logger = $crate::logging::MAIN_LOGGER.lock().unwrap();
-        let pid = (*logger).pid;
-        let tag = &*$crate::logging::TAG;
-
-        let (now, precise_time) = $crate::sozu_command_lib::logging::now();
-        (*logger).log(
-            &_META,
-            format_args!(
-                concat!("{} {} {} {} {}\t", $format, '\n'),
-                now, precise_time, pid, tag,
-                $level_tag $(, $final_args)*)
-            );
-      }
-    });
-    ($lvl:expr, $format:expr, $level_tag:expr $(, $args:expr)+) => {
-      log!(__inner__ module_path!(), $lvl, $format, $level_tag, [], [a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v]
-                  $(, $args)+)
-    };
-    ($lvl:expr, $format:expr, $level_tag:expr) => {
-      log!(__inner__ module_path!(), $lvl, $format, $level_tag, [], [a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v])
-    };
-}
-
-pub fn init(
-    tag: String,
-    spec: &str,
-    backend: LoggerBackend,
-    access_backend: Option<LoggerBackend>,
-) {
-    let directives = crate::sozu_command_lib::logging::parse_logging_spec(spec);
-    let mut logger = MAIN_LOGGER.lock().unwrap();
-    if !logger.initialized {
-        logger.set_directives(directives);
-        logger.backend = backend;
-        logger.access_backend = access_backend;
-        logger.tag = tag;
-        logger.pid = unsafe { libc::getpid() };
-        logger.initialized = true;
-
-        log::set_max_level(log::LevelFilter::Info);
-    }
-}
-
-pub fn setup(tag: String, level: &str, target: &str, access_target: Option<&str>) {
-    let backend = target_to_backend(target);
-    let access_backend = access_target.map(target_to_backend);
-
-    if let Ok(log_level) = env::var("RUST_LOG") {
-        init(tag, &log_level, backend, access_backend);
-    } else {
-        // We set the env variable so every worker can access it
-        env::set_var("RUST_LOG", level);
-        init(tag, level, backend, access_backend);
-    }
-    //initialize TAG here to avoid deadlocks
-    let _ = &*TAG;
-}

bin/src/main.rs
@@ -24,8 +24,6 @@
 //! to Sōzu via its UNIX socket.
 
 #[macro_use]
-extern crate lazy_static;
-#[macro_use]
 extern crate prettytable;
 #[macro_use]
 extern crate sozu_lib as sozu;
@@ -37,9 +35,6 @@ extern crate num_cpus;
 #[cfg(feature = "jemallocator")]
 #[global_allocator]
 static ALLOC: jemallocator::Jemalloc = jemallocator::Jemalloc;
-
-#[macro_use]
-mod logging;
 
 /// the arguments to the sozu command line
 mod cli;
@@ -63,7 +58,7 @@ use libc::{cpu_set_t, pid_t};
 #[cfg(target_os = "linux")]
 use regex::Regex;
 use sozu::metrics::METRICS;
-use sozu_command_lib::config::Config;
+use sozu_command_lib::{config::Config, logging::setup_logging_with_config};
 use std::panic;
 
 use crate::worker::{get_executable_path, start_workers, Worker};
@@ -122,7 +117,7 @@ fn start(args: &cli::Args) -> Result<(), anyhow::Error> {
     let config_file_path = get_config_file_path(args)?;
     let config = load_configuration(config_file_path)?;
 
-    util::setup_logging(&config, "MAIN");
+    setup_logging_with_config(&config, "MAIN");
     info!("Starting up");
     util::setup_metrics(&config).with_context(|| "Could not setup metrics")?;
     util::write_pid_file(&config).with_context(|| "PID file is not writeable")?;

bin/src/upgrade.rs
@@ -16,8 +16,8 @@ use serde::{Deserialize, Serialize};
 use tempfile::tempfile;
 
 use sozu_command_lib::{
-    channel::Channel, config::Config, proto::command::RunState, request::WorkerRequest,
-    state::ConfigState,
+    channel::Channel, config::Config, logging::setup_logging_with_config, proto::command::RunState,
+    request::WorkerRequest, state::ConfigState,
 };
 
 use crate::{command::CommandServer, util, worker::Worker};
@@ -155,7 +155,7 @@ pub fn begin_new_main_process(
         serde_json::from_reader(upgrade_file).with_context(|| "could not parse upgrade data")?;
     let config = upgrade_data.config.clone();
 
-    util::setup_logging(&config, "MAIN");
+    setup_logging_with_config(&config, "MAIN");
     util::setup_metrics(&config).with_context(|| "Could not setup metrics")?;
     //info!("new main got upgrade data: {:?}", upgrade_data);
 

bin/src/util.rs
@@ -7,8 +7,6 @@ use nix::fcntl::{fcntl, FcntlArg, FdFlag};
 use sozu::metrics;
 use sozu_command_lib::config::Config;
 
-use crate::logging;
-
 pub fn enable_close_on_exec(fd: RawFd) -> Result<i32, anyhow::Error> {
     let file_descriptor =
         fcntl(fd, FcntlArg::F_GETFD).with_context(|| "could not get file descriptor flags")?;
@@ -34,16 +32,6 @@ pub fn disable_close_on_exec(fd: RawFd) -> Result<i32, anyhow::Error> {
     new_flags.remove(FdFlag::FD_CLOEXEC);
 
     fcntl(fd, FcntlArg::F_SETFD(new_flags)).with_context(|| "could not set file descriptor flags")
-}
-
-pub fn setup_logging(config: &Config, tag: &str) {
-    //FIXME: should have an id for the main too
-    logging::setup(
-        tag.to_owned(),
-        &config.log_level,
-        &config.log_target,
-        config.log_access_target.as_deref(),
-    );
 }
 
 pub fn setup_metrics(config: &Config) -> anyhow::Result<()> {

bin/src/worker.rs
@@ -29,7 +29,7 @@ use sozu::{metrics, server::Server};
 use sozu_command_lib::{
     channel::Channel,
     config::Config,
-    logging::target_to_backend,
+    logging::setup_logging_with_config,
     proto::command::{request::RequestType, Request, RunState, Status, WorkerInfo},
     ready::Ready,
     request::{read_requests_from_file, WorkerRequest},
@@ -38,7 +38,7 @@ use sozu_command_lib::{
     state::ConfigState,
 };
 
-use crate::{logging, util};
+use crate::util;
 
 /// An instance of Sōzu, as seen from the main process
 pub struct Worker {
@@ -225,39 +225,23 @@ pub fn begin_worker_process(
 
     let mut configuration_state_file = unsafe { File::from_raw_fd(configuration_state_fd) };
 
-    let initial_state = read_requests_from_file(&mut configuration_state_file)
-        .with_context(|| "could not parse configuration state data")?;
-
     let worker_config = worker_to_main_channel
         .read_message()
         .with_context(|| "worker could not read configuration from socket")?;
 
     let worker_id = format!("{}-{:02}", "WRK", id);
-    logging::setup(
-        worker_id.clone(),
-        &worker_config.log_level,
-        &worker_config.log_target,
-        worker_config.log_access_target.as_deref(),
-    );
+
+    // do not try to log anything before this, or the logger will panic
+    setup_logging_with_config(&worker_config, &worker_id);
 
     trace!(
         "Creating worker {} with config: {:#?}",
         worker_id,
         worker_config
     );
-
-    let backend = target_to_backend(&worker_config.log_target);
-    let access_backend = worker_config
-        .log_access_target
-        .as_deref()
-        .map(target_to_backend);
-    sozu_command_lib::logging::Logger::init(
-        worker_id.clone(),
-        &worker_config.log_level,
-        backend,
-        access_backend,
-    );
     info!("worker {} starting...", id);
+    let initial_state = read_requests_from_file(&mut configuration_state_file)
+        .with_context(|| "could not parse configuration state data")?;
 
     if let Err(e) = worker_to_main_channel.nonblocking() {
         error!("Could not unblock the worker-to-main channel: {}", e);

command/src/logging.rs
@@ -14,9 +14,11 @@ use libc;
 use mio::net::UnixDatagram;
 use rand::{distributions::Alphanumeric, thread_rng, Rng};
 
+use crate::config::Config;
+
 thread_local! {
   pub static LOGGER: RefCell<Logger> = RefCell::new(Logger::new());
-  pub static TAG:    String          = LOGGER.with(|logger| logger.borrow().tag.clone());
+  pub static TAG: String = LOGGER.with(|logger| {logger.borrow().tag.clone()});
 }
 
 pub static COMPAT_LOGGER: CompatLogger = CompatLogger;
@@ -482,6 +484,38 @@ pub fn parse_logging_spec(spec: &str) -> Vec<LogDirective> {
     }
 
     dirs
+}
+
+/// start the logger from config (takes RUST_LOG into account)
+pub fn setup_logging_with_config(config: &Config, tag: &str) {
+    setup_logging(
+        &config.log_target,
+        config.log_access_target.as_deref(),
+        &config.log_level,
+        tag,
+    )
+}
+
+/// start the logger, after:
+///
+/// - determining logging backends
+/// - taking RUST_LOG into account
+pub fn setup_logging(
+    log_target: &str,
+    log_access_target: Option<&str>,
+    log_level: &str,
+    tag: &str,
+) {
+    let backend = target_to_backend(log_target);
+    let access_backend = log_access_target.map(target_to_backend);
+
+    if let Ok(env_log_level) = env::var("RUST_LOG") {
+        Logger::init(tag.to_string(), &env_log_level, backend, access_backend);
+    } else {
+        // We set the env variable so every worker can access it
+        env::set_var("RUST_LOG", log_level);
+        Logger::init(tag.to_string(), log_level, backend, access_backend);
+    }
 }
 
 pub fn target_to_backend(target: &str) -> LoggerBackend {

lib/examples/main.rs
@@ -5,14 +5,14 @@ extern crate sozu_lib;
 extern crate sozu_command_lib;
 extern crate time;
 
-use std::{env, io::stdout, thread};
+use std::thread;
 
 use anyhow::Context;
 
 use sozu_command_lib::{
     channel::Channel,
     config::ListenerBuilder,
-    logging::{Logger, LoggerBackend},
+    logging::setup_logging,
     proto::command::{
         request::RequestType, AddBackend, AddCertificate, CertificateAndKey, LoadBalancingParams,
         PathRule, RequestHttpFrontend,
@@ -21,21 +21,7 @@ use sozu_command_lib::{
 };
 
 fn main() -> anyhow::Result<()> {
-    if env::var("RUST_LOG").is_ok() {
-        Logger::init(
-            "EXAMPLE".to_string(),
-            &env::var("RUST_LOG").with_context(|| "could not get the RUST_LOG env var")?,
-            LoggerBackend::Stdout(stdout()),
-            None,
-        );
-    } else {
-        Logger::init(
-            "EXAMPLE".to_string(),
-            "info",
-            LoggerBackend::Stdout(stdout()),
-            None,
-        );
-    }
+    setup_logging("stdout", None, "info", "EXAMPLE");
 
     info!("MAIN\tstarting up");
 

lib/examples/minimal.rs
@@ -3,7 +3,7 @@ extern crate time;
 #[macro_use]
 extern crate sozu_command_lib;
 
-use std::{collections::BTreeMap, env, io::stdout, thread};
+use std::{collections::BTreeMap, thread};
 
 use anyhow::Context;
 
@@ -11,7 +11,7 @@ use sozu_command_lib::{
     channel::Channel,
     config::ListenerBuilder,
     info,
-    logging::{Logger, LoggerBackend},
+    logging::setup_logging,
     proto::command::{
         request::RequestType, AddBackend, Cluster, LoadBalancingAlgorithms, LoadBalancingParams,
         PathRule, RequestHttpFrontend, RulePosition,
@@ -21,21 +21,7 @@ use sozu_command_lib::{
 };
 
 fn main() -> anyhow::Result<()> {
-    if env::var("RUST_LOG").is_ok() {
-        Logger::init(
-            "EXAMPLE".to_string(),
-            &env::var("RUST_LOG").with_context(|| "could not get the RUST_LOG env var")?,
-            LoggerBackend::Stdout(stdout()),
-            None,
-        );
-    } else {
-        Logger::init(
-            "EXAMPLE".to_string(),
-            "info",
-            LoggerBackend::Stdout(stdout()),
-            None,
-        );
-    }
+    setup_logging("stdout", None, "info", "EXAMPLE");
 
     info!("starting up");
 

lib/examples/tcp.rs
@@ -4,12 +4,12 @@ extern crate sozu_lib;
 extern crate sozu_command_lib;
 extern crate time;
 
-use std::{io::stdout, thread};
+use std::thread;
 
 use anyhow::Context;
 use sozu_command_lib::{
     channel::Channel,
-    logging::{Logger, LoggerBackend},
+    logging::setup_logging,
     proto::command::{
         request::RequestType, AddBackend, LoadBalancingParams, RequestTcpFrontend,
         TcpListenerConfig,
@@ -18,19 +18,7 @@ use sozu_command_lib::{
 };
 
 fn main() -> anyhow::Result<()> {
-    /*
-    if env::var("RUST_LOG").is_ok() {
-     Logger::init("EXAMPLE".to_string(), &env::var("RUST_LOG").with_context(|| "could not get the RUST_LOG env var"), LoggerBackend::Stdout(stdout()));
-    } else {
-     Logger::init("EXAMPLE".to_string(), "info", LoggerBackend::Stdout(stdout()));
-    }
-    */
-    Logger::init(
-        "EXAMPLE".to_string(),
-        "debug",
-        LoggerBackend::Stdout(stdout()),
-        None,
-    );
+    setup_logging("stdout", None, "info", "EXAMPLE");
 
     info!("starting up");
 
@@ -45,12 +33,7 @@ fn main() -> anyhow::Result<()> {
             address: "127.0.0.1:8080".parse().expect("could not parse address"),
             ..Default::default()
         };
-        Logger::init(
-            "TCP".to_string(),
-            "debug",
-            LoggerBackend::Stdout(stdout()),
-            None,
-        );
+        setup_logging("stdout", None, "debug", "TCP");
         sozu_lib::tcp::start_tcp_worker(listener, max_buffers, buffer_size, channel);
     });
 

lib/src/lib.rs
@@ -223,7 +223,7 @@
 //!     channel::Channel,
 //!     config::ListenerBuilder,
 //!     info,
-//!     logging::{Logger, LoggerBackend},
+//!     logging::setup_logging,
 //!     proto::command::{
 //!         request::RequestType, AddBackend, Cluster, LoadBalancingAlgorithms, LoadBalancingParams,
 //!         PathRule, Request, RequestHttpFrontend, RulePosition,
@@ -232,21 +232,7 @@
 //! };
 //!
 //! fn main() -> anyhow::Result<()> {
-//!     if env::var("RUST_LOG").is_ok() {
-//!         Logger::init(
-//!             "EXAMPLE".to_string(),
-//!             &env::var("RUST_LOG").with_context(|| "could not get the RUST_LOG env var")?,
-//!             LoggerBackend::Stdout(stdout()),
-//!             None,
-//!         );
-//!     } else {
-//!         Logger::init(
-//!             "EXAMPLE".to_string(),
-//!             "info",
-//!             LoggerBackend::Stdout(stdout()),
-//!             None,
-//!         );
-//!     }
+//!     setup_logging("stdout", None, "info", "EXAMPLE");
 //!
 //!     info!("starting up");
 //!

e2e/src/sozu/worker.rs
@@ -1,6 +1,4 @@
 use std::{
-    env,
-    io::stdout,
     net::SocketAddr,
     os::unix::prelude::{AsRawFd, FromRawFd, IntoRawFd},
     thread::{self, JoinHandle},
@@ -15,7 +13,7 @@ use sozu::server::Server;
 use sozu_command::{
     channel::Channel,
     config::{Config, ConfigBuilder, FileConfig},
-    logging::{Logger, LoggerBackend},
+    logging::setup_logging,
     proto::command::{
         request::RequestType, AddBackend, Cluster, HardStop, LoadBalancingParams, PathRule,
         Request, RequestHttpFrontend, RequestTcpFrontend, ReturnListenSockets, RulePosition,
@@ -161,13 +159,7 @@ impl Worker {
         println!("Setting up logging");
 
         let server_job = thread::spawn(move || {
-            let log_level = env::var("RUST_LOG").unwrap_or("error".to_string());
-            Logger::init(
-                thread_name.to_owned(),
-                &log_level,
-                LoggerBackend::Stdout(stdout()),
-                None,
-            );
+            setup_logging("stdout", None, "error", &thread_name);
             let mut server = Server::try_new_from_config(
                 cmd_worker_to_main,
                 thread_scm_worker_to_main,

e2e/src/tests/tests.rs
@@ -1,5 +1,4 @@
 use std::{
-    io::stdout,
     net::SocketAddr,
     thread,
     time::{Duration, Instant},
@@ -7,8 +6,8 @@ use std::{
 
 use sozu_command_lib::{
     config::{FileConfig, ListenerBuilder},
-    info,
-    logging::{Logger, LoggerBackend},
+    info, log,
+    logging::setup_logging,
     proto::command::{
         request::RequestType, ActivateListener, AddCertificate, CertificateAndKey, ListenerType,
         RemoveBackend, RequestHttpFrontend,
@@ -633,13 +632,7 @@ pub fn try_hard_or_soft_stop(soft: bool) -> State {
 }
 
 fn try_http_behaviors() -> State {
-    use sozu_command_lib::log;
-    Logger::init(
-        "BEHAVE-OUT".to_string(),
-        "debug",
-        LoggerBackend::Stdout(stdout()),
-        None,
-    );
+    setup_logging("stdout", None, "debug", "BEHAVE-OUT");
 
     info!("starting up");
 
@@ -1077,13 +1070,7 @@ pub fn try_blue_geen() -> State {
 }
 
 pub fn try_keep_alive() -> State {
-    use sozu_command_lib::log;
-    Logger::init(
-        "KA-OUT".to_string(),
-        "debug",
-        LoggerBackend::Stdout(stdout()),
-        None,
-    );
+    setup_logging("stdout", None, "debug", "KA-OUT");
 
     let front_address = create_local_address();
 